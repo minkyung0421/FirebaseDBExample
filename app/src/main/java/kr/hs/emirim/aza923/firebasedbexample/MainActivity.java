efd48dfe06b5342439053b4c23e71b928fb94c60
minkyung0421 <aza923@e-mirim.hs.kr>
2017-09-19 20:52:58 +0900

    #5 웹 브라우져로 링크 띄우기 구현

--- app/src/main/java/kr/hs/emirim/aza923/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/aza923/firebasedbexample/MainActivity.java
@@ -1,14 +1,17 @@
 package kr.hs.emirim.aza923.firebasedbexample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView mNameTextView;
     private TextView mGithubTextView;
@@ -28,5 +31,16 @@ public class MainActivity extends AppCompatActivity {
                 .centerCrop()
                 .resize(100,100)
                 .into(mProfileImageView);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.profile_github:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse(mGithubTextView.getText().toString() );
+                intent.setData(uri);
+                startActivity(intent);
+                break;
+        }
     }
 }
