fdbe608d7cbd7b123c232e55d5e515847d2a1fbf
minkyung0421 <aza923@e-mirim.hs.kr>
2017-09-19 20:29:42 +0900

    #4 picasso와의 만남

--- app/src/main/java/kr/hs/emirim/aza923/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/aza923/firebasedbexample/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
         mNameTextView = (TextView) findViewById(R.id.profile_name);
         mGithubTextView = (TextView) findViewById(R.id.profile_github);
         mProfileImageView = (ImageView) findViewById(R.id.profile_image);
+
+        Picasso.with(this)
+                .load("http://i.imgur.com/DvpvklR.png")
+                .centerCrop()
+                .resize(100,100)
+                .into(mProfileImageView);
     }
 }
